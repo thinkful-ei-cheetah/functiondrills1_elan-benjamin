a986859975f2abd634b5bc1ac1d819a218271eca
Ben Tilghman <ben.tilghman@gmail.com>
2019-03-25 14:51:55 -0400

    add try/catch

--- funcdrills1.js
+++ funcdrills1.js
@@ -1,13 +1,22 @@
 'use strict';
 
 function createGreeting(name, age) {
-  return 'Hi my name is Elan and I am 24.';
+  const ageCalc = getYearofBirth(age);
+  return `Hi my name is Elan and I am ${ageCalc}`;
 }
 
 function getYearofBirth(age){
+  if (age <= 0) {
+    throw new Error('Age can not be negative');
+  }
   return 2019 - age;
 }
 
-const greeting1 = createGreeting();
-console.log(greeting1);
+
+try {
+  const greeting1 = createGreeting('Ben', 15);
+  console.log(greeting1);
+} catch(e) {
+  console.log('Age can not be negative');
+}
 
