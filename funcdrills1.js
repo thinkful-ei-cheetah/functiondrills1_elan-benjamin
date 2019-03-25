a6ff352d3f4f9dead19b129783c43fa2e56214ef
Ben Tilghman <ben.tilghman@gmail.com>
2019-03-25 14:40:36 -0400

    separate yearOfBirth calc into own function

--- funcdrills1.js
+++ funcdrills1.js
@@ -1,8 +1,11 @@
 'use strict';
 
 function createGreeting(name, age) {
-  const yearOfBirth = 2019 - age;
-  return 'Hi my name is Elan and I am 24. I was born in ${yearOfBirth}';
+  return 'Hi my name is Elan and I am 24.';
+}
+
+function getYearofBirth(age){
+  return 2019 - age;
 }
 
 const greeting1 = createGreeting();
