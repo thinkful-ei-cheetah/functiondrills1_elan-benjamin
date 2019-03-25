b36f113a53759552a81e88835ddae74b7576d383
Elan Green <green.elan5@gmail.com>
2019-03-25 14:33:02 -0500

    added beyond function

--- funcdrills2.js
+++ funcdrills2.js
@@ -6,3 +6,14 @@ function jediName(firstName, lastName) {
   return lastName.slice(0, 3) + firstName.slice(0,2);
 }
 
+function beyond(num) {
+  if(num === Infinity) {
+    console.log('And beyond');
+  } else if (num > 0) {
+    console.log('To infity');
+  } else if (num < 0) {
+    console.log('To negative infinity');
+  } else if (num === 0) {
+    console.log('Staying home');
+  }
+}
